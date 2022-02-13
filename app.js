93bcc41d5b0bc2f9f9c2d4bdd8cb95ce0e52fb0f
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-13 15:13:23 +0600

    product items and price rate self update

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-function productNumber(product, isadd) {
+
+
+function productPrice(product, productNumber, price){
+    const productPriceText = document.getElementById(product +'-price');
+    const productPrice = productPriceText.innerText;
+    const newPrice =  price * productNumber;
+    productPriceText.innerText = newPrice;
+}
+
+function productNumber(product, isadd, price) {
     const productNumberText = document.getElementById(product + '-number');
     const prductNumber = productNumberText.value;
     let increaseNumber=0;
@@ -7,30 +16,34 @@ function productNumber(product, isadd) {
     } else if (parseInt(prductNumber) > 0) {
         increaseNumber = parseInt(prductNumber) - 1;
     }
-    
     productNumberText.value = increaseNumber;
+    // product price update it-self 
+    productPrice(product,increaseNumber,price);
+    // total product price and tax update 
+    totalPrice ();
+
 };
 
 
 document.getElementById('mobile-plus').addEventListener('click', function () {
 
-    productNumber('mobile', true);
+    productNumber('mobile', true, 1219);
 });
 
 document.getElementById('mobile-minus').addEventListener('click', function () {
 
 
-    productNumber("mobile", false);
+    productNumber("mobile", false, 1219);
 });
 
 document.getElementById('case-plus').addEventListener('click', function () {
 
-    productNumber("case", true);
+    productNumber("case", true, 59);
 });
 
 document.getElementById('case-minus').addEventListener('click', function () {
 
 
-    productNumber('case', false);
+    productNumber('case', false, 59);
 });
 
